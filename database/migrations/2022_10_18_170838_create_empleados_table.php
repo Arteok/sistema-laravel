0525b11d54643ca191b98827af639cee2f426b57
Pablo Mariasch <103390999+Arteok@users.noreply.github.com>
2022-10-18 16:02:37 -0300

    18-10-2022

--- database/migrations/2022_10_18_170838_create_empleados_table.php
+++ database/migrations/2022_10_18_170838_create_empleados_table.php
@@ -13,14 +13,14 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('failed_jobs', function (Blueprint $table) {
-            $table->id();
-            $table->string('uuid')->unique();
-            $table->text('connection');
-            $table->text('queue');
-            $table->longText('payload');
-            $table->longText('exception');
-            $table->timestamp('failed_at')->useCurrent();
+        Schema::create('empleados', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('Nombre' ,10);
+            $table->string('ApellidoPaterno' ,10);
+            $table->string('ApellidoMaterno' ,10);
+            $table->string('Correo');
+            $table->string('Foto');
+            $table->timestamps();
         });
     }
 
@@ -31,6 +31,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('failed_jobs');
+        Schema::dropIfExists('empleados');
     }
 };
